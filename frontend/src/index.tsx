febc694a9759ed72f5f29cf09de792c450af9b4e
sseiryu <seiryu1026@gmail.com>
2024-05-07 23:41:17 +0900

    mainをbranch1にマージするため

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Test from './test';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
