399873e3dc5af51dc935a5d6241f66b5401e0c49
Tsukubasan <wing.rsw.283283.nigabi@gmail.com>
2024-05-07 14:57:22 +0900

    Create a minimam api

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
+theme_list = ["東工大", "大岡山"]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get("/theme")
+async def get_theme():
+    index = random.randrange(0, len(theme_list), 1)
+    return {
+        "theme": theme_list[index]
+    }
 
 # pip install uvicorn fastapi
 #  uvicorn main:app --reload で起動
