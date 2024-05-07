b076b2b6ea70b51190e23c2013f778268ff72bd8
Hirose-Yuto <hirose.y.aj@m.titech.ac.jp>
2024-05-07 14:39:40 +0900

    create routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Top from "./top/Top";
+import Settings from "./settings/Settings";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* ヘッダー、すべての画面に表示される*/}
+            <header>
+                <Link to={"/"}>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        トップ
+                    </button>
+                </Link>
+                <Link to={"/settings"}>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        設定
+                    </button>
+                </Link>
+            </header>
+
+            {/* パスごとに表示するコンポーネントを変える */}
+            <Routes>
+                <Route path='/' element={<Top/>}/>
+                <Route path='/settings' element={<Settings/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
