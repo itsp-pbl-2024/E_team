d0ff5468730543379001cabc7c6c42b0e190f0a5
TsukasaYa <115986372+TsukasaYa@users.noreply.github.com>
2024-05-07 16:27:44 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,37 @@
 from fastapi import FastAPI
 import random
+<<<<<<< HEAD
 
 app = FastAPI()
 
 odais = ["東工大", "東京工業大学", "東京工科大学", "日本工業大学", "科学大", "医科歯科大", "デジタルハリウッド大"]
+=======
+from fastapi.middleware.cors import CORSMiddleware
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+>>>>>>> origin/main
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+theme_list = ["東工大", "大岡山"]
+
+@app.get("/theme")
+async def get_theme():
+    index = random.randrange(0, len(theme_list), 1)
+    return {
+        "theme": theme_list[index]
+    }
 
 @app.get("/odai")
 async def odai(id: int = None):
