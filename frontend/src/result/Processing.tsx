1f74c63593f63cc12f4ec8cb5804f4af5c52bfd7
Tsukubasan <wing.rsw.283283.nigabi@gmail.com>
2024-07-09 15:08:34 +0900

    implement result scene

---
+++ frontend/src/result/Processing.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Processing() {
+    return (
+        <div className='text-2xl p-32'>
+            判定中...
+        </div>
+    );
+};
+
+export default Processing;
